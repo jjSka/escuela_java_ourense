6366e7aa0c6e245b2af386ee53c712929aeeb224
JJ <t_mac055@hotmail.com>
2019-11-06 15:52:16 +0100

    Empezando: HolaMundo.java

---
+++ 00_IntroJava/HolaMundo.java
@@ -0,0 +1,9 @@
+class HolaMundo{
+	public static void main(String[] args){
+		byte unByte=127;
+		int unEntero=1000;
+		System.out.println("Hola Ourense");
+		System.out.println("Byte: "+unByte);
+		System.out.println("Entero: "+unEntero);
+	}
+}
